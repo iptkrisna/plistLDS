68034691d5275d435e76b52245062e2f4b405223
iptkrisna <krisnamath9@gmail.com>
2019-06-28 18:23:15 +0700

    Initial Commit

---
+++ plistLDS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  plistLDS
+//
+//  Created by I Putu Krisna on 28/06/19.
+//  Copyright © 2019 I Putu Krisna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
